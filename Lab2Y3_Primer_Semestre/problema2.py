b29812e576720d18159f3ecfebd2e1f1a74cc025
0rcan <josue.zambrano@correounivalle.edu.co>
2023-10-17 19:55:45 -0500

    Modificaciones

--- Lab2Y3_Primer_Semestre/problema2.py
+++ Lab2Y3_Primer_Semestre/problema2.py
@@ -4,28 +4,28 @@
 # Número de grupo: 50
 # Laboratorio 2 y 3
 
-def data(age, mounths):
+def data(age, months):
 
     if age < 12 and age > 0:
         category = "infant"
-        value_mounth = 43000
+        value_month = 43000
     elif age >= 12 and age < 18:
         category = "young"
-        value_mounth = 36000
+        value_month = 36000
     elif age >= 18 and age < 100:
         category = "senior"
-        value_mounth = 32000
+        value_month = 32000
     
     return print(f"""
     Name: {name}
     Category: {category}
-    Value to pay: {value_mounth * mounths}
+    Value to pay: {value_month * months}
     """)    
     
 if __name__ == '__main__':
     name = input("\n Enter a name: ")
     age = int(input(" register your age: "))
-    mounths = int(input(" Mounts of inscripcion: "))
-    data(age, mounths)
+    months = int(input(" Mounts of inscripcion: "))
+    data(age, months)
 
 
