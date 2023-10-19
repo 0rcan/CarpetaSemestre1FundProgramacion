a7185ea08b52a95c529294cd3ad0d979be803312
0rcan <josue.zambrano@correounivalle.edu.co>
2023-10-18 21:24:16 -0500

    Modificaciones finales al Problema4, talvez lo modifique en el futuro

--- Lab2Y3_Primer_Semestre/problema4.py
+++ Lab2Y3_Primer_Semestre/problema4.py
@@ -4,12 +4,7 @@
 # Número de grupo: 50
 # Laboratorio 2 y 3
 
-def final_note ():
-    
-    result1 = float((exam_partial1 * 0.2) + (homework1 * 0.2) + (homework2 * 0.2))
-    result2 = float((final_exam * 0.4) + (share * 0.2))
-    calculation = float(result1 + result2)
-
+def fundamentos ():
     if calculation >= 0.0 and calculation < 2.0:
         category = "Bad"
     elif calculation >= 2.0 and calculation < 3.0:
@@ -20,7 +15,49 @@ def final_note ():
         category = "Good"
     elif calculation >= 4.5 and calculation <= 5.0:
         category = "Excelent"
-        
+    return print(f"""
+        Student name: {estudent_name}
+        Subject name= {subject_name} 
+        Clasification= {category}
+        """)
+    
+def calculoI ():
+    if calculation >= 0.0 and calculation < 2.0:
+        category = "Bad"
+    elif calculation >= 2.0 and calculation < 3.0:
+        category = "Deficient"
+    elif calculation >= 3.0 and calculation < 3.5:
+        category = "Regular"
+    elif calculation >= 3.5 and calculation < 4.5:
+        category = "Good"
+    elif calculation >= 4.5 and calculation <= 5.0:
+        category = "Excelent"
+    return print(f"""
+        Student name: {estudent_name}
+        Subject name= {subject_name} 
+        Clasification= {category}
+        """)
+def inglesI ():
+    if calculation >= 0.0 and calculation < 3.0:
+        category = "Bad"
+    elif calculation >= 3.0 and calculation < 4.0:
+        category = "Deficient"
+    elif calculation >= 4.0 and calculation < 4.5:
+        category = "Regular"
+    elif calculation >= 4.5 and calculation <= 5.0:
+        category = "Good"
+    return print(f"""
+        Student name: {estudent_name}
+        Subject name= {subject_name} 
+        Clasification= {category}
+        """)
+def deporte ():
+    if calculation >= 0.0 and calculation < 3.0:
+        category = "Bad"
+    elif calculation >= 3.0 and calculation < 4.0:
+        category = "Regular"
+    elif calculation >= 4.0 and calculation <= 5.0:
+        category = "Good"
     return print(f"""
         Student name: {estudent_name}
         Subject name= {subject_name} 
@@ -35,4 +72,23 @@ if __name__ == '__main__':
     homework2 = float(input(" Homework II: ")) #20
     final_exam = float(input(" Final exam: ")) #40
     share = float(input(" Share: ")) #20
-    final_note()
+    
+    result1 = float((exam_partial1 * 0.2) + (homework1 * 0.1) + (homework2 * 0.1))
+    result2 = float((final_exam * 0.4) + (share * 0.2))
+    calculation = float(result1 + result2)
+    
+    if subject_name == "Fundamentos":
+        fundamentos()
+    elif subject_name == "CalculoI":
+        calculoI()
+    elif subject_name == "InglesI":
+        inglesI()
+    elif subject_name == "Deporte":
+        deporte()
+    else:
+        print("""    
+    *****************
+    SUBJECT INCORRECT
+
+    *****************
+    """)
