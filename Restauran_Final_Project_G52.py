cb216f502d1a7140b02dde1dee19471e85425fb7
0rcan <josue.zambrano@correounivalle.edu.co>
2023-11-12 22:05:30 -0500

    Se crea el proyecto restaurante

---
+++ Restauran_Final_Project_G52.py
@@ -0,0 +1,10 @@
+# JOSUE JACINTO ZAMBRANO LOAIZA 2380741-3743
+# Integrante2: PrimerNombre SegundoApellido – código2
+# Integrante2: PrimerNombre SegundoApellido – código3
+
+# Docente: Luis Germán Toro Pareja
+# Número de grupo: 52
+# Proyecto Final restaurante
+
+import tkinter
+
