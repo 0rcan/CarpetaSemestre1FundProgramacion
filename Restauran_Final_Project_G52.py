427a3bdc2df9a316fa1890b91d7959ef57a9de13
0rcan <josue.zambrano@correounivalle.edu.co>
2023-11-15 16:04:25 -0500

    Se creo la ventana y los botones del home

--- Restauran_Final_Project_G52.py
+++ Restauran_Final_Project_G52.py
@@ -6,5 +6,20 @@
 # Número de grupo: 52
 # Proyecto Final restaurante
 
-import tkinter
+import tkinter as tk
+# Creacion de la Ventana
+window = tk.Tk()
+window.geometry("700x700")
+window.title("Mi Restaurante")
 
+text = tk.Label(window, text="Mi Restaurante")
+text.configure(font=("arial", 17)) # Estilos del texto
+text.grid(row=1, column=1) # .grid Imprime el elemento
+
+button = tk.Button(window, text= "to register")
+button.grid(row=4, column=7)
+
+button2 = tk.Button(window, text= "log in")
+button2.grid(row=5, column=7)
+
+window.mainloop() # Permite mantener en siclo el programa
