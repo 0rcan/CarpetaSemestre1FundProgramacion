ccba494d3fcee5376d8229d471c448f502037e73
0rcan <josue.zambrano@correounivalle.edu.co>
2023-12-13 19:25:17 -0500

    Co-authored-by: A75TD <A75TD@users.noreply.github.com>

--- pruebapestañastk.py
+++ pruebapestañastk.py
@@ -10,12 +10,100 @@ window_dishes.title("Gestión de platos")
 pestañas = ttk.Notebook(window_dishes)
 pestañas.grid(row=0,column=0)
 pestaña1 = ttk.Frame(pestañas,padding=125)
-pestaña2 = ttk.Frame(pestañas)
+
 pestaña3 = ttk.Frame(pestañas)
 
 pestañas.add(pestaña1,text="Titulo")
-pestañas.add(pestaña2,text="Titulo2")
 pestañas.add(pestaña3,text="Titulo3")
+
+
+#========================================================================
+#========================================================================
+#========================================================================
+def x():
+    pestaña2 = ttk.Frame(pestañas)
+    pestañas.add(pestaña2,text="Titulo2")
+    # Contenedor principal
+    main_container_dishesB = tk.Frame(pestaña2)
+    main_container_dishesB.grid(row=0, column=0)
+
+    # Titulo y logo
+    tittle_dishesB = tk.Label(
+        main_container_dishesB, text="Mi Restaurante"
+    )
+    tittle_dishesB.configure(
+        font=("fuente_negrita",20)  # Estilos del texto
+        ) 
+    # Imprime el elemento con determinada posición
+    tittle_dishesB.grid(row=0, column=0)
+    # Logo
+    image_homeE = tk.PhotoImage(file="restaurante.png")
+    image_homeE = image_homeE.subsample(10)  # Tamaño de la imagen
+    lbl_image_homeE = tk.Label(
+        main_container_dishesB, image=image_homeE
+    ).grid(
+        row=2, column=0,pady=5
+    )
+
+    # Sub contenedor dentro del contenedor principal
+    sub_container_dishesB = tk.Frame(main_container_dishesB)
+
+    sub_container_dishesB.grid(row=3, column=0, pady=15)
+    font_boldB = ("Arial", 13, "bold")
+    sub_container_dishesB.configure(bg="#5E5C6C",padx=10)
+
+    # titulo de entradas
+    text_dishesB = tk.Label(
+        sub_container_dishesB, text="Agregar platos",font=(
+            "fuente_negrita",14
+            ),bg="#5E5C6C",fg="#ffffff"
+        )
+    text_dishesB.grid(row=3, columnspan=2, pady=7)
+
+    # Entradas
+    # Entrada nombres
+    name_dishes = tk.Label(
+        sub_container_dishesB, text="Nombre",bg="#5E5C6C",fg="#ffffff"
+        )
+    name_dishes.grid(row=4, column=0)
+    name_entry_dishes = tk.Entry(sub_container_dishesB)
+    name_entry_dishes.grid(row=5, column=0, pady=5,padx=15)
+
+    # Entrada precios
+    price_dishes = tk.Label(
+        sub_container_dishesB, text="Precio",bg="#5E5C6C",fg="#ffffff"
+        )
+    price_dishes.grid(row=4, column=1)
+    price_entry_dishes = tk.Entry(sub_container_dishesB)
+    price_entry_dishes.grid(row=5, column=1)
+
+    # Entrada descripción
+    description_dishes = tk.Label(
+        sub_container_dishesB, text="Descripción",bg="#5E5C6C",fg="#ffffff"
+        )
+    description_dishes.grid(row=6, column=0,pady=4)
+    description_entry_dishes = tk.Entry(sub_container_dishesB)
+    description_entry_dishes.grid(row=7, column=0)
+
+    # Entrada disponibilidad
+    availability_dishes = tk.Label(
+        sub_container_dishesB, text="Disponibilidad",
+        bg="#5E5C6C",fg="#ffffff"
+        )
+    availability_dishes.grid(row=6, column=1,pady=4)
+    availability_entry_dishes = tk.Entry(sub_container_dishesB)
+    availability_entry_dishes.grid(row=7, column=1)
+
+    # Ttitulo botón
+    button_entry_dishes = tk.Button(
+        sub_container_dishesB, text="Agregar",bg="gray",fg="#ffffff" # Boton registro
+    )
+    # Botón
+    button_entry_dishes.grid(row=8, columnspan=2, pady=20)
+
+
+
+
 # Contenedor principal
 main_container_dishes = tk.Frame(pestaña1)
 main_container_dishes.grid(row=1, column=1)
@@ -60,7 +148,7 @@ text_dishes.grid(row=3, column=0)
 
 # Botones gestión de platos
 button_dishesA = tk.Button(
-    sub_container_dishes, text="Agregar",bg="gray",fg="#ffffff" # Boton registro
+    sub_container_dishes, text="Agregar",bg="gray",fg="#ffffff",command=x # Boton registro
 )
 button_dishesA.grid(row=4, column=0,pady=10)
 button_dishesB = tk.Button(
@@ -72,89 +160,6 @@ button_dishesC = tk.Button(
 )
 button_dishesC.grid(row=6, column=0,pady=10)
 
-
-
-#========================================================================
-#========================================================================
-#========================================================================
-
-# Contenedor principal
-main_container_dishesB = tk.Frame(pestaña2)
-main_container_dishesB.grid(row=0, column=0)
-
-# Titulo y logo
-tittle_dishesB = tk.Label(
-    main_container_dishesB, text="Mi Restaurante"
-)
-tittle_dishesB.configure(
-    font=("fuente_negrita",20)  # Estilos del texto
-    ) 
-# Imprime el elemento con determinada posición
-tittle_dishesB.grid(row=0, column=0)
-# Logo
-image_homeE = tk.PhotoImage(file="restaurante.png")
-image_homeE = image_homeE.subsample(10)  # Tamaño de la imagen
-lbl_image_homeE = tk.Label(
-    main_container_dishesB, image=image_homeE
-).grid(
-    row=2, column=0,pady=5
-)
-
-# Sub contenedor dentro del contenedor principal
-sub_container_dishesB = tk.Frame(main_container_dishesB)
-
-sub_container_dishesB.grid(row=3, column=0, pady=15)
-font_boldB = ("Arial", 13, "bold")
-sub_container_dishesB.configure(bg="#5E5C6C",padx=10)
-
-# titulo de entradas
-text_dishesB = tk.Label(
-    sub_container_dishesB, text="Agregar platos",font=(
-        "fuente_negrita",14
-        ),bg="#5E5C6C",fg="#ffffff"
-    )
-text_dishesB.grid(row=3, columnspan=2, pady=7)
-
-# Entradas
-# Entrada nombres
-name_dishes = tk.Label(
-    sub_container_dishesB, text="Nombre",bg="#5E5C6C",fg="#ffffff"
-    )
-name_dishes.grid(row=4, column=0)
-name_entry_dishes = tk.Entry(sub_container_dishesB)
-name_entry_dishes.grid(row=5, column=0, pady=5,padx=15)
-
-# Entrada precios
-price_dishes = tk.Label(
-    sub_container_dishesB, text="Precio",bg="#5E5C6C",fg="#ffffff"
-    )
-price_dishes.grid(row=4, column=1)
-price_entry_dishes = tk.Entry(sub_container_dishesB)
-price_entry_dishes.grid(row=5, column=1)
-
-# Entrada descripción
-description_dishes = tk.Label(
-    sub_container_dishesB, text="Descripción",bg="#5E5C6C",fg="#ffffff"
-    )
-description_dishes.grid(row=6, column=0,pady=4)
-description_entry_dishes = tk.Entry(sub_container_dishesB)
-description_entry_dishes.grid(row=7, column=0)
-
-# Entrada disponibilidad
-availability_dishes = tk.Label(
-    sub_container_dishesB, text="Disponibilidad",
-    bg="#5E5C6C",fg="#ffffff"
-    )
-availability_dishes.grid(row=6, column=1,pady=4)
-availability_entry_dishes = tk.Entry(sub_container_dishesB)
-availability_entry_dishes.grid(row=7, column=1)
-
-# Ttitulo botón
-button_entry_dishes = tk.Button(
-    sub_container_dishesB, text="Agregar",bg="gray",fg="#ffffff" # Boton registro
-)
-# Botón
-button_entry_dishes.grid(row=8, columnspan=2, pady=20)
 
 
 #========================================================================
@@ -193,7 +198,7 @@ sub_container_tableB.configure(bg="#5E5C6C",padx=10)
 
 # titulo de entradas
 text_dtableB = tk.Label(
-    sub_container_tableB, text="Agregar platos",font=(
+    sub_container_tableB, text="Agregar mesa",font=(
         "fuente_negrita",14
         ),bg="#5E5C6C",fg="#ffffff"
     )
